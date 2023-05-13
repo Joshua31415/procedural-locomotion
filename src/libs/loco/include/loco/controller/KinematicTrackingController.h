4c12731e28527497eb0897c4ad6b56daa3d5cacc
JoshuaAurand <aurandj@student.ethz.ch>
2023-05-13 14:52:09 +0200

    restructure controller

--- src/libs/loco/include/loco/controller/KinematicTrackingController.h
+++ src/libs/loco/include/loco/controller/KinematicTrackingController.h
@@ -17,17 +17,20 @@ public:
     std::shared_ptr<LeggedRobot> robot;
     std::shared_ptr<IK_Solver> ikSolver = nullptr;
 
-    double cycle = 0;
-    P3D rFootTarget;
-    P3D rFootFinal;
-    P3D rFootInitial;
-    P3D lFootTarget;
-    P3D lFootFinal;
-    P3D lFootInitial;
-    bool rFootSet = false;
-    bool lFootSet = false;
-    bool lLocked = false;
-    bool rLocked = false;
+    double t = 0;
+    double cycleLength = 1;
+    double stanceStart = 0.0;
+    double stanceEnd = 0.5;
+    double swingStart = 0.5;
+    double swingEnd = 1.0;
+
+    std::array<P3D, 2> stanceTargets;
+    std::array<P3D, 2> swingTargets;
+    std::array<Trajectory3D, 2> swingTrajectories;
+    std::array<std::array<int, 4>, 2> footJointIndices;
+
+    enum Phase {Stance, Swing};
+    int its = 0;
 
 public:
     /**
@@ -36,6 +39,26 @@ public:
     KinematicTrackingController(const std::shared_ptr<LocomotionTrajectoryPlanner> &planner) : LocomotionController(planner) {
         this->robot = planner->robot;
         ikSolver = std::make_shared<IK_Solver>(robot);
+
+        for (int i = 0; i < 2; ++i) {
+            P3D p = robot->getLimb(i)->getEEWorldPos();
+            footTargets[i] = p;
+            footInitials[i] = p;
+            footFinals[i] = p; 
+            footLocked[i] = true;
+            stanceTargets[i] = p;
+            swingTrajectories[i].addKnot(0, V3D(p));
+            swingTrajectories[i].addKnot(1, V3D(p));
+        }
+
+        std::array<char *, 4> lJoints = {"lHip_1", "lKnee", "lAnkle_1", "lToeJoint"};
+        std::array<char *, 4> rJoints = {"rHip_1", "rKnee", "rAnkle_1", "rToeJoint"};
+        for (int i = 0; i < 4; ++i) {
+            int lIdx = robot->getJointIndex(lJoints[i]);
+            int rIdx = robot->getJointIndex(rJoints[i]);
+            footJointIndices[0][i] = rIdx;
+            footJointIndices[1][i] = lIdx;
+        }
     }
 
     /**
@@ -53,37 +76,114 @@ public:
         // follow target base trajectory.
         P3D targetPos = planner->getTargetTrunkPositionAtTime(planner->getSimTime() + dt);
         Quaternion targetOrientation = planner->getTargetTrunkOrientationAtTime(planner->getSimTime() + dt);
-
         robot->setRootState(targetPos, targetOrientation);
 
-        // std::cout << cycle << "\n";
-        auto lFoot = robot->getLimb(1);
-        auto rFoot = robot->getLimb(0);
-        
-        if (lLocked) {
-            // stance phase
-            cycle += dt;
-            if (cycle >= 1.0) {
-                lLocked = false;
-                lFootInitial = lFoot->getEEWorldPos();
-                cycle = cycle - int(cycle);
-            }
-        } else {
-            // swing phase
-            double frac = std::min(std::max(2.0 * cycle, 0.0), 1.0);
-            lFootFinal = lFoot->limbRoot->getWorldCoordinates() + lFoot->defaultEEOffsetWorld;
-            lFootTarget = lFootInitial * (1.0 - frac) + lFootFinal * frac;
+        t += dt;
+        // use the toe as the targeted endeffector in the stance phase
+        // use the heel as the targeted endeffector in the swing phase
+        // idx i == 0 <-> right leg
+        for (int i = 0; i < 2; ++i) {
+            auto toes = robot->getLimb(i);
+            auto ankle = robot->getLimb(i);
 
+            Phase currentPhase = getPhase(i, t);
+            Phase nextPhase = getPhase(i, t + dt);
 
-            cycle += dt;
-            if (cycle >= 0.5) {
-                lFootTarget = lFoot->getEEWorldPos();
-                lLocked = true;
+            if (currentPhase == Phase::Stance && nextPhase == Phase::Stance) {
+                // foot stays still during stance phase
+                setStanceTarget(i);
+                ikSolver->solve();
+                fixToeAngle(i);
+            } else if (currentPhase == Phase::Stance && nextPhase == Phase::Swing) {
+                computeSwingTrajectory(i);
+                setSwingTarget(i);
+                ikSolver->solve();
+            } else if (currentPhase == Phase::Swing && nextPhase == Phase::Swing) {
+                setSwingTarget(i);
+                ikSolver->solve();
+            } else if (currentPhase == Phase::Swing && nextPhase == Phase::Stance) {
+                // foot stays still during stance phase
+                computeStanceTarget(i);
+                setStanceTarget(i);
+                ikSolver->solve();
+                fixToeAngle(i);
             }
         }
 
-        ikSolver->addEndEffectorTarget(lFoot->eeRB, lFoot->ee->endEffectorOffset, lFootTarget);
-        ikSolver->solve();
+    }
+
+    void computeSwingTrajectory(int i) {
+        auto foot = robot->getLimb(i);
+
+        swingTrajectories[i].clear();
+        P3D pStart = foot->getEEWorldPos();
+        V3D offset(pStart, foot->limbRoot->getWorldCoordinates() + foot->defaultEEOffsetWorld);
+        P3D pEnd = pStart + offset * 2;
+        swingTrajectories[i].addKnot(0, V3D(pStart));
+        swingTrajectories[i].addKnot(1, V3D(pEnd));
+    }
+
+    void setSwingTarget(int i) {
+        auto foot = robot->getLimb(i);
+        double cyclePercent = getCyclePercent(i, t);
+        P3D swingTarget = getP3D(swingTrajectories[i].evaluate_linear(remap(cyclePercent, swingStart, swingEnd)));
+        ikSolver->addEndEffectorTarget(foot->eeRB, foot->ee->endEffectorOffset, swingTarget);
+    }
+
+    void computeStanceTarget(int i) {
+        auto foot = robot->getLimb(i);
+        stanceTargets[i] = foot->getEEWorldPos();
+    }
+    void setStanceTarget(int i) {
+        auto foot = robot->getLimb(i);
+        ikSolver->addEndEffectorTarget(foot->eeRB, foot->ee->endEffectorOffset, stanceTargets[i]);
+    }
+
+    double remap(double t, double a, double b) {
+        // remap t in [a, b] to [0, 1]
+        return (t - a) / (b - a);
+    }
+
+    P3D lerp(P3D a, P3D b, double t) {
+        return a * (1.0 - t) + b * t;
+    }
+
+    double clamp(double t, double a, double b) {
+        return std::min(std::max(t, a), b);
+    }
+
+    double getCyclePercent(int i, double t) {
+        return std::fmod(i * 0.5 * cycleLength + t, cycleLength) / cycleLength;
+    }
+
+    bool isStance(double cyclePercent) {
+        return stanceStart <= cyclePercent && cyclePercent < stanceEnd;
+    }
+
+    bool isSwing(double cyclePercent) {
+        return swingStart <= cyclePercent && cyclePercent < swingEnd;
+    }
+
+    Phase getPhase(int i, double t) {
+        double cyclePercent = getCyclePercent(i, t);
+        if (isStance(cyclePercent)) {
+            return Phase::Stance;
+        } else if (isSwing(cyclePercent)) {
+            return Phase::Swing;
+        }
+    }
+
+    void fixToeAngle(int footIdx) {
+        GeneralizedCoordinatesRobotRepresentation gcrr(robot);
+        dVector q;
+        gcrr.getQ(q);
+        double angle = 0;
+        for (int i = 0; i < 3; ++i) {
+            angle += q(footJointIndices[footIdx][i] + 6);
+        }
+        q(6 + footJointIndices[footIdx][3]) = -angle;
+        gcrr.setQ(q);
+        gcrr.syncRobotStateWithGeneralizedCoordinates();
     }
 
     void advanceInTime(double dt) override {
