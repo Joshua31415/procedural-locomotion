254e231eacfcdcc9719ca0f947c2205db04a8af2
Marie <marie.jaillot@hotmail.com>
2023-05-22 13:25:17 +0200

    Basic movement

--- src/libs/loco/include/loco/planner/SimpleLocomotionTrajectoryPlanner.h
+++ src/libs/loco/include/loco/planner/SimpleLocomotionTrajectoryPlanner.h
@@ -95,7 +95,7 @@ public:
     }
 
     virtual P3D getTargetTrunkPositionAtTime(double t) {
-        return P3D() + bFrameMotionPlan.bFramePosTrajectory.evaluate_linear(t);  // +RBGlobals::worldUp.cross(robot->forward) * 0.05 * sin(5 * t);
+        return P3D() + bFrameMotionPlan.bFramePosTrajectory.evaluate_linear(t) + RBGlobals::worldUp * 0.03 * sin(4 * M_PI * t) - P3D(0, 0.03, 0);
     }
 
     virtual double getTargetTrunkHeadingAtTime(double t) {
